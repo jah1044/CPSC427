084c11faaff04ddd47d765f08d7399388facf1da
jah1044 <jhudak2004@gmail.com>
2026-03-25 16:45:29 -0400

    updated db config

--- photo editor/Backend/config/db.js
+++ photo editor/Backend/config/db.js
@@ -1,6 +1,8 @@
+// [03/25/2026] load env + mysql
 require("dotenv").config();
 const mysql = require("mysql2");
 
+// [03/25/2026] create db connection
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
@@ -8,6 +10,7 @@ const db = mysql.createConnection({
   database: process.env.DB_NAME
 });
 
+// [03/25/2026] connect to mysql
 db.connect((err) => {
   if (err) {
     console.error("Database connection failed:", err);
